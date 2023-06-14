192f0bc45b9c8c4dee5b847c1ad9421784e97f1e
Hannah Collins <hannahcollins53@gmail.com>
2023-06-14 14:28:21 -0700

    styling

--- src/flight/Transportation.js
+++ src/flight/Transportation.js
@@ -23,9 +23,14 @@ export const TransportationList = () => {
         evt.preventDefault()
         setTripId(evt.target.value)
     }
-    return <>List of Transportation
-    <div>Which Trip do you want view transportation for?</div>
-    <select onClick={(evt) => GrabTrip(evt)}>
+    console.log(transportation)
+    return <>
+    <div className="border-b-2 border-midnightBlue shadow-md">
+        <img src="https://www.nec.com/en/global/solutions/transportation/images/transportation_header_pc.jpg"/>
+    </div>
+    <article className="mx-auto flex items center justify-center flex-col">
+    <div className="text-center mt-6 text-2xl text-maroonBrown">Which Trip do you want view transportation for?</div>
+    <select className="w-56 rounded-lg mx-auto m-4" onClick={(evt) => GrabTrip(evt)}>
      <option name= "trip">Choose a Trip</option>
 
         {
@@ -36,15 +41,42 @@ export const TransportationList = () => {
         )})
         }
     </select>
-    <div>
+    <div className="overflow-x-auto mx-auto">
+        <table className="table">
+        <thead>
+      <tr>
+        <th></th>
+        <th>Mode of Transportation</th>
+        <th>Departure Location</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Arrival Location</th>
+        <th>Time</th>
+      </tr>
+    </thead>
+    <tbody>
         {
             transportation.length > 0 ?  transportation.map(transport => {
-                return <><div> {transport.transport_type}</div>
-                <div></div> </>
+                return <> <tr>
+                <th></th>
+                <td>{transport.transport_type}</td>
+                <td>{transport.dep_location.name}</td>
+                <td>{transport.dep_date}</td>
+                <td>{transport.dep_time}</td>
+                <td>{transport.arr_location.name}</td>
+                <td>{transport.arr_time}</td>
+              </tr></>
             }): ""
         }
+</tbody>
+</table>
     </div>
-    <Link to={`/transportForm/${tripId}`}>Add new transportation</Link>
-    <a href={"https://www.google.com/travel/flights"} target="_blank">Search for Flights?</a>
+    
+    <Link className="btn w-52 mx-auto" to={`/transportForm/${tripId}`}>Add new transportation</Link>
+    <a className="underline text-right mr-10" href={"https://www.google.com/travel/flights"} target="_blank">Need to search for Flights?</a>
+    </article>
+    
     </>
 }
+
+// possible icons?
--- src/itinerary/ItineraryList.js
+++ src/itinerary/ItineraryList.js
@@ -1,29 +1,75 @@
-import { useEffect, useState } from "react"
-import { getTrips, getUserTrips } from "./TripProvider"
+import React, { useEffect, useState } from "react"
+import { getUserTrips } from "./TripProvider"
 import { Link } from "react-router-dom"
 
 export const Itineraries = () => {
-    const[itineraries, setItineraries] = useState([])
+    const [itineraries, setItineraries] = useState([]);
+  
     useEffect(() => {
-        getUserTrips().then((data) => {
-            setItineraries(data)
-        })
-    }, [])
-    return<>My Itineraries
-    <div className="grid grid-cols-5 gap-4">
-    {
-        itineraries.map(itinerary => {
-            return <> 
-            <div className=" grid col-auto">
-                <Link to={`/itinerary/${itinerary.id}`}> <img className="w-[200px]" src={itinerary.image}/> </Link>
-            </div>
-            </>
-        })
-    }
-    </div>
-     <Link className="btn" to="/planning">
-                Plan a new trip
-            </Link>
-
-    </>
-}
+      getUserTrips().then((data) => {
+        setItineraries(data);
+      });
+    }, []);
+  
+    const changeGridSize = (index) => {
+      if ((index + 1) % 2 === 0) {
+        return { width: "200px", height: "300px" };
+      } else {
+        return { width: "175px", height: "200px" };
+      }
+    };
+  
+    const changeGrid = (index) => {
+      if ((index + 1) % 2 === 0) {
+        return "grid-cols-2 md:grid-cols-2 lg:grid-cols-3";
+    } else {
+      return "grid-cols-1";
+      }
+    };
+  
+    const changeImageSize = (index) => {
+      if ((index + 1) % 2 === 0) {
+        return "h-[300px] w-[200px] rounded-lg";
+      } else {
+        return "h-[200px] w-[175px] rounded-lg";
+      }
+    };
+  
+  
+    const renderGridItems = () => {
+      return itineraries.map((itinerary, index) => (
+        <div className="relative">
+             <a class="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+             <Link to={`/itinerary/${itinerary.id}`}> <h1  className="tracking-wider" >{itinerary.name}</h1></Link>
+      </a>
+      <div className="relative">
+        <div
+          className={`ml-2 mb-2 ${changeGrid(index)}`}
+          key={itinerary.id}
+          style={changeGridSize(index)}
+        >
+          <Link to={`/itinerary/${itinerary.id}`}>
+            <img
+              className={changeImageSize(index)}
+              src={itinerary.image}
+              alt=""
+            />
+          </Link>
+        </div>
+        </div>
+        </div>
+      ));
+    };
+  
+    return (
+      <>
+        {/* <h1>My Itineraries</h1> */}
+        <div className="bg-tan h-screen">
+        <div className="flex flex-wrap px-32 items-center justify-center pt-10">{renderGridItems()}</div>
+        <Link className="btn ml" to="/planning">
+          Plan a new trip
+        </Link>
+        </div>
+      </>
+    );
+  };
